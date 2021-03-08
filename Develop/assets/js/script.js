0e08f53a85825c1c79771570d773b31b3a6b102b
Duality <jramirezarroyo21@gmail.com>
2021-03-07 23:53:02 -0800

    Text Input Color

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -8,35 +8,108 @@ function showTime() {
 
 showTime();
 
-//function setUp() {
+function coloringIn() {
+    
+    var rightNowInHours = new Date().getHours();
 
-//     $(document).ready(function () {
+    if(rightNowInHours > 8) {
+        $("#eventColumn9").addClass("past");
+    }
+    else if(rightNowInHours >= 8 && rightNowInHours < 9) {
+        $("#eventColumn9").addClass("present");
+    }
+    else if(rightNowInHours < 8) {
+        $("#eventColumn9").addClass("future");
+    }
 
-       
+    if(rightNowInHours > 9) {
+        $("#eventColumn10").addClass("past");
+    }
+    else if(rightNowInHours >= 9 && rightNowInHours < 10) {
+        $("#eventColumn10").addClass("present");
+    }
+    else if(rightNowInHours < 9) {
+        $("#eventColumn10").addClass("future");
+    }
 
-//         function displayBlocks() {
-//             //Array of Work Hours to label blocks
-//             let workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+    if(rightNowInHours > 10) {
+        $("#eventColumn11").addClass("past");
+    }
+    else if(rightNowInHours >= 10 && rightNowInHours < 11) {
+        $("#eventColumn11").addClass("present");
+    }
+    else if(rightNowInHours < 10) {
+        $("#eventColumn11").addClass("future");
+    }
 
-//             let row;
-//             let timeSquare;
-//             let eventColumn;
-//             let saveBtn;
+    if(rightNowInHours > 11) {
+        $("#eventColumn12").addClass("past");
+    }
+    else if(rightNowInHours >= 11 && rightNowInHours < 12) {
+        $("#eventColumn12").addClass("present");
+    }
+    else if(rightNowInHours < 11) {
+        $("#eventColumn12").addClass("future");
+    }
 
-//             for(let i = 0; i < workHours.length; i++) {
-//                 let row = $("<div>").addClass("row");
-//                 let timeSquare = $("<text-area>").text(workHours[i]).addClass("hour");
-//                 let eventColumn = $("<input>").addClass("time-block");
-//                 let saveBtn = $("<button>").addClass("saveBtn").text("Save");
-//                 $(row).append(timeSquare).append(eventColumn).append(saveBtn);
-//                 $("#schedule").append(row);
-//             }
+    if(rightNowInHours > 12) {
+        $("#eventColumn1").addClass("past");
+    }
+    else if(rightNowInHours >= 12 && rightNowInHours < 13) {
+        $("#eventColumn1").addClass("present");
+    }
+    else if(rightNowInHours < 12) {
+        $("#eventColumn1").addClass("future");
+    }
 
-//             //Loop to label Blocks
+    if(rightNowInHours > 13) {
+        $("#eventColumn2").addClass("past");
+    }
+    else if(rightNowInHours >= 13 && rightNowInHours < 14) {
+        $("#eventColumn2").addClass("present");
+    }
+    else if(rightNowInHours < 13) {
+        $("#eventColumn2").addClass("future");
+    }
 
+    if(rightNowInHours > 14) {
+        $("#eventColumn2").addClass("past");
+    }
+    else if(rightNowInHours >= 14 && rightNowInHours < 15) {
+        $("#eventColumn2").addClass("present");
+    }
+    else if(rightNowInHours < 14) {
+        $("#eventColumn2").addClass("future");
+    }
 
-//         }
+    if(rightNowInHours > 15) {
+        $("#eventColumn3").addClass("past");
+    }
+    else if(rightNowInHours >= 15 && rightNowInHours < 16) {
+        $("#eventColumn3").addClass("present");
+    }
+    else if(rightNowInHours < 15) {
+        $("#eventColumn3").addClass("future");
+    }
 
-//         displayBlocks();
-//     })
-// }
+    if(rightNowInHours > 16) {
+        $("#eventColumn4").addClass("past");
+    }
+    else if(rightNowInHours >= 16 && rightNowInHours < 17) {
+        $("#eventColumn4").addClass("present");
+    }
+    else if(rightNowInHours < 16) {
+        $("#eventColumn4").addClass("future");
+    }
+    if(rightNowInHours > 17) {
+        $("#eventColumn5").addClass("past");
+    }
+    else if(rightNowInHours >= 17 && rightNowInHours < 18) {
+        $("#eventColumn5").addClass("present");
+    }
+    else if(rightNowInHours < 17) {
+        $("#eventColumn5").addClass("future");
+    }
+
+ }
+ coloringIn();
